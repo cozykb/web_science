be4f58ff385d2f566460433fd5ab77386bba4bf7
cozykb <57888375+cozykb@users.noreply.github.com>
2022-03-20 20:43:09 +0100

    fix the bug for unstable function Prediction.__user_mean_vector

--- summary.py
+++ summary.py
@@ -26,7 +26,7 @@ class Preprocess:
         self.item_df = item_df
         self.training_data, self.test_data = self.__split_ui_data()
         self.clean_item_data = self.__clean_item_data()
-        self.ui_matrix = self.__ui_matrix_training
+        self.ui_matrix = self.__ui_matrix_training()
 
     class Predection:
         def __init__(self, uid, iid, est):
@@ -86,7 +86,7 @@ class Collaborativefiltering_recommendation(Preprocess):
         return result
 
     def cos_sim_prediction(self, user_id: str, item_id: str, rank: int):
-        result = Collaborativefiltering_recommendation.cos_sim_ui(
+        result = self.cos_sim_ui(
             user_id, item_id)
         result_sort = result.sort_values(
             by='cos_sim', ascending=False).head(rank)
@@ -97,14 +97,7 @@ class Collaborativefiltering_recommendation(Preprocess):
         return prediction
 
     def simple_svd(self, user_id: str, item_id: str, k: int, mute: bool):
-        matrix = pd.DataFrame(columns=self.training_data['asin'].drop_duplicates(
-        ), index=self.training_data['reviewerID'].drop_duplicates())
-        for row_i in range(len(self.training_data)):
-            reviewerID = list(self.training_data.reviewerID)[row_i]
-            asin = list(self.training_data.asin)[row_i]
-            rate = list(self.training_data.overall)[row_i]
-            matrix.loc[reviewerID, asin] = rate
-        matrix = matrix.fillna(0)
+        matrix = self.ui_matrix
         for index in matrix.index:
             mean_ = np.mean([i for i in matrix.loc[index] if i != 0])
             if index == user_id:
@@ -179,6 +172,7 @@ class Evaluation(Preprocess):
         for i, name in enumerate(self.predection_list):
             if name.uid not in users_in_pred_but_not_in_test:
                 out_list.append(self.predection_list[i])
+        # print(len(out_list))
         return out_list
 
     def top_prediction(self, rank, uid):
@@ -253,9 +247,6 @@ class Evaluation(Preprocess):
             'hr_k': self.hr_k_user
         }
         function_ = switch[function_name]
-        user_list = []
-        user_list = [
-            item.uid for item in self.predection_list if item.uid not in user_list]
         num_users = len(self.ui_relevant_matrix.index)
         summation = []
         user_item_rating = defaultdict(list)
@@ -264,8 +255,16 @@ class Evaluation(Preprocess):
         for user_id, filted_pred_list in user_item_rating.items():
             filted_pred_list.sort(key=lambda x: x[1], reverse=True)
             summation.append(function_(filted_pred_list, user_id,
-                             cut_off, self.ui_relevant_matrix))
+                             cut_off))
         return sum(summation)/float(num_users)
+
+    def rank_k(self,uid:str,rank:int)-> list:
+        user_item_rating = defaultdict(list)
+        for pred in self.predection_list:
+            user_item_rating[pred.uid].append((pred.iid, pred.est))
+        for user_id, filted_pred_list in user_item_rating.items():
+            filted_pred_list.sort(key=lambda x: x[1], reverse=True)
+        return user_item_rating[uid][:rank]
 
 
 class Contentbased_recommendation(Preprocess):
@@ -275,6 +274,7 @@ class Contentbased_recommendation(Preprocess):
         self.original_title = list(self.clean_item_data['title'])
         self.clean_title = self.__clean_title()
         self.tfidf = self.__tfidf()
+        self.user_mean_vector = self.__user_mean_vector()
 
     def __clean_title(self):
         porter_stemmer = PorterStemmer()
@@ -291,28 +291,46 @@ class Contentbased_recommendation(Preprocess):
         x = tfidf_vectorizer.fit_transform(self.clean_title)
         return pd.DataFrame(x.toarray(), columns=tfidf_vectorizer.get_feature_names_out(), index=self.clean_item_data['asin'])
 
-    def user_mean_vector(self):
-        for uid in self.training_data['reviewerID']:
+    def __user_mean_vector(self):
+        user_mean_vector_dic = {}
+        # print(user_list)
+        for uid in set(self.training_data['reviewerID']):
             item_in = self.clean_item_data['asin'][self.clean_item_data['asin'].isin(
                 self.training_data[self.training_data['reviewerID'] == uid]['asin'])]
-            for item in item_in:
-                pass
-        pass
+            summation = 0
+            temp_list = []
+            for iid in item_in:
+                temp_list.append(self.tfidf.loc[iid,:].to_numpy()*int(self.ui_matrix.loc[uid, iid]))
+                summation += int(self.ui_matrix.loc[uid, iid])
+            user_mean_vector_dic[uid] = np.sum(temp_list, axis=0)/summation        
+        return user_mean_vector_dic
 
-    def __predection(self):
-        def x(bools): return True if bools == False else False
-        for uid in self.training_data['reviewerID']:
-            item_in = self.clean_item_data['asin'][self.clean_item_data['asin'].isin(
-                self.training_data[self.training_data['reviewerID'] == uid]['asin'])]
+    def pick_item_from_tfidf(self, item_list:list):
+            vector_list = []
+            for item in item_list:
+                vector_list.append(self.tfidf.loc[item,:].to_numpy())
+            return vector_list
+    
+    def predection(self):
+        x = lambda bools: True if bools == False else False
+        predection_list = []
+        # print(user_list)
+        # chick_1 = 0
+        for uid in set(self.training_data['reviewerID']):
             item_not_in = self.clean_item_data['asin'][[x(bools) for bools in self.clean_item_data['asin'].isin(
                 self.training_data[self.training_data['reviewerID'] == uid]['asin'])]]
-
+            # chick_1 += len(item_not_in)
+            vector_not_in = self.pick_item_from_tfidf(item_not_in)
+            cos_sim = cosine_similarity(vector_not_in,[self.user_mean_vector[uid]])
+            for i,item in enumerate(item_not_in):
+                predection_list.append(self.Predection(uid=uid, iid=item, est=cos_sim[i]))
+        # print(chick_1)
+        # print(len(predection_list))
+        return predection_list
 
 if __name__ == '__main__':
     import gzip
-    import os
     import json
-    import re
     import pandas as pd
 
     def getDF(path):
@@ -330,4 +348,8 @@ if __name__ == '__main__':
     df_1 = getDF('All_Beauty_5.json.gz')
     df_2 = pd.read_json('meta_All_Beauty.json', lines=True)
     preprocess = Contentbased_recommendation(ui_df=df_1, item_df=df_2)
-    print(preprocess.test_data)
+    predicetion_list = preprocess.predection()
+    evaluation = Evaluation(predicetion_list, df_1, df_2)
+    kr_mean = evaluation.mean_k(5, 'hr_k')
+    print(evaluation.rank_k(uid='A39WWMBA0299ZF',rank=20))
+    print(kr_mean)
